ac475c46cd4725b92cf1a0ff551f8b779fcdaee2
shashi prakash singh <singhshashi09871@gmail.com>
2025-09-11 21:09:14 +0530

    added usestate hooks for state management

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
     const [number, setNumber] = useState("");
     const [result, setResult] = useState("");
-
+ 
   return (
     <>
       <div>
